7d480df65265b15163f652cdc156525a69accd13
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2024-01-03 14:01:16 +0800

    progress bar refcatoring

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,16 +4,23 @@ const QuestionTimer = ({ timeout, onTimeout }) => {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [onTimeout, timeout]);
 
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prev) => prev - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
-  return <progress id="question_time" max={timeout} value={remainingTime}/>;
+  return <progress id="question_time" max={timeout} value={remainingTime} />;
 };
 
 export default QuestionTimer;
